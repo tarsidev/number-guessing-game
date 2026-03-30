48539b476e9ba85098d20bf3bbec519c2d06d370
Tarsila <tarsilabarrosfritz05@gmail.com>
2026-03-30 20:47:08 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿class Program()
+{
+    public static void Main()
+    {
+        // generate a random number
+        // asks for the user input
+        // if userInput is write: user win, if not: user loose
+
+        int randomNum = 10;
+
+        Console.Write("Inform your guess: ");
+        int userGuess = Convert.ToInt32(Console.ReadLine());
+
+        if (userGuess == randomNum) {
+            Console.WriteLine("You won!");
+        } else
+        {
+            Console.WriteLine("Try again.");
+        }
+    }
+}
